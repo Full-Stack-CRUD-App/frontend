40b570fb07556e2d70bf094725304adf116703f9
kathrynzaro <zarokathryn@gmail.com>
2022-10-11 13:00:40 -0700

    items

--- src/services/items.js
+++ src/services/items.js
@@ -3,7 +3,7 @@ import { del, get, post, put } from './request.js';
 const URL = 'https://full-stack-crud-backend.herokuapp.com';
 
 export async function getItems() {
-  return await get(`${URL}`);
+  return await get(`${URL}/api/v1/items`);
 }
 
 export async function createItem(item) {
